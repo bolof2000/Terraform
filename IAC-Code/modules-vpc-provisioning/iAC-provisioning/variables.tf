4be44cd638e4f2c2eea8232993ebf098180db24d
Bolofinde Olusegun <bolofbaba@MacBook-Pro-2.local>
2021-03-16 22:45:43 -0600

    IAC

---
+++ IAC-Code/modules-vpc-provisioning/iAC-provisioning/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "aws-vpc" {
+
+}
+variable "public_subnet_1_cidr" {}
+variable "public_subnet_2_cidr" {}
+variable "public_subnet_3_cidr" {}
+variable "private_subnet_1_cidr" {}
+variable "private_subnet_2_cidr" {}
+variable "private_subnet_3_cidr" {}
